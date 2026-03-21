a781e12fcbd28a2383c530dd9612617c65532986
Martin Montes <martin11lrx@gmail.com>
2026-03-21 19:20:19 +0100

    Updated LLM examples

---
+++ examples/llm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl -sS -X POST https://facebook-test.mmontes-internal.duckdns.org/v1/completions   \
+  -H 'accept: application/json'   \
+  -H 'Content-Type: application/json'    \
+  -d '{
+      "model": "facebook/opt-125m",
+      "prompt":"Who let the dogs out?"
+    }' | jq
